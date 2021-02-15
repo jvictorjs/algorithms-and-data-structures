e7aed832e9e5c90d9c40b2911ca17571eddebfc7
J Victor <jvwr.cin@gmail.com>
2021-02-15 19:03:16 -0300

    Create 1.3 Warmup - Compare the Triplets.js

---
+++ hackerrank/algorithms/1.3 Warmup - Compare the Triplets.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let alicePoints = 0
+    let bobPoints = 0
+    for (let i in a){
+        alicePoints += a[i] > b[i] ? 1 : 0
+        bobPoints += b[i] > a[i] ? 1 : 0
+    }
+    return [alicePoints, bobPoints]
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
